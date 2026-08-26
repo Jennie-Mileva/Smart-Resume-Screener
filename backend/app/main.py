741b5e7fd6b8562bfa90877806355e635b6cba37
Jennie Mileva <jenniemileva@gmail.com>
2026-08-27 02:21:23 +0530

    Prepare backend and frontend fro deployement

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,11 @@
+import os
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.app.api.resume import router as resume_router
 from backend.app.api.candidates import router as candidates_router
-from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI(
     title="Smart Resume Screener",
@@ -10,16 +13,29 @@ app = FastAPI(
     version="1.0.0",
 )
 
+
+frontend_url = os.getenv(
+    "FRONTEND_URL",
+    "http://127.0.0.1:5505",
+)
+
+allowed_origins = [
+    "http://127.0.0.1:5505",
+    "http://localhost:5505",
+]
+
+if frontend_url not in allowed_origins:
+    allowed_origins.append(frontend_url)
+
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:5505",
-        "http://localhost:5505",
-    ],
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 app.include_router(resume_router)
 app.include_router(candidates_router)
